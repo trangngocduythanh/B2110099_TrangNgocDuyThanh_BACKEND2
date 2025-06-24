785b9c39f5c5ccb62346ecc9a4bcbe6be40f2aa5
trangngocduythanhh <trangngocduythanhh@example.com>
2025-06-25 04:51:52 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 // Khoi tao ung dung, mot the hien cua express
 const app = express();
@@ -15,6 +16,17 @@ app.use("/api/contacts", contactRouter);
 // Dinh nghia cac route
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
+});
+
+// Middleware xu ly loi
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal server Error",
+    });
 });
 
 // Xuat module app de co the su dung o file khac
